9045e07a957a198b99d1dff5a9126052cabdab66
Fiona <fiona_berkery@hotmail.com>
2022-03-31 15:20:26 +0100

    friends q4

--- start_point/src/friends.py
+++ start_point/src/friends.py
@@ -4,3 +4,15 @@ def get_name(person):
 def get_favourite_tv_show(tv_show):
     return tv_show["favourites"]["tv_show"]
     
+def likes_to_eat(person,food_to_find):
+    found_food = False
+    for food in person["favourites"]["snacks"]:
+        if food == food_to_find:
+            found_food = True
+    return found_food
+
+def add_friend(person, friend):
+    person["friends"].append(friend)
+
+
+        
